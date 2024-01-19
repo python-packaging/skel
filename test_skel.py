1d48b4a506ca2033633469fcd708020600e71626
Tim Hatch <tim@timhatch.com>
2024-01-18 17:23:07 -0800

    Sync with 3.7+ code

---
+++ test_skel.py
@@ -0,0 +1,14 @@
+import unittest
+import regen
+
+class RegenTest(unittest.TestCase):
+    def test_variable_format(self):
+        self.assertEqual("1", regen.variable_format("{foo}", foo="1"))
+        self.assertEqual("{foo}", regen.variable_format("{foo}"))
+        self.assertEqual("{ foo }", regen.variable_format("{ foo }", foo="1"))
+        self.assertEqual("{{foo}}", regen.variable_format("{{foo}}", foo="1"))
+        with self.assertRaises(TypeError):
+            self.assertEqual("1", regen.variable_format("{foo}", foo=1, bar=2))
+
+if __name__ == "__main__":
+    unittest.main()
